6083b5aa3242f145a3fa2316688e923946e915b9
JeremJ <jeremiasz.jaworski@gmail.com>
2019-06-25 15:44:57 +0200

    restrict /users endpoint

--- src/main/java/com/bs/library/config/SecurityConfig.java
+++ src/main/java/com/bs/library/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import com.bs.library.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -57,7 +58,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().
                 authorizeRequests()
-                .antMatchers("/users/**").permitAll()
+                .antMatchers(HttpMethod.POST,"/users/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
